45b6baccc393a5f2507e28c7f18f42a1256c5c7e
sonu.tiwari <sonu.t@turing.com>
2023-06-29 01:53:20 +0530

    Added react solution

--- react_js_solution/src/App.js
+++ react_js_solution/src/App.js
@@ -1,22 +1,56 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [selection, setSelection] = useState({ selectionStart: 0, selectionEnd: 0 });
+
+  const handleInputChange = (event) => {
+    event.preventDefault();
+    event.stopPropagation();
+    let value = event.target.value;
+    const { selectionStart, selectionEnd } = event.target;
+    if (value.length < inputValue.length) { // deletion case.
+      console.log({ selectionStart: selectionStart, selectionEnd: selectionEnd });
+      setInputValue(value);
+      return;
+    }
+    value = value.replace(/\D/g, "");
+    const length = value.length;
+    if (length < 3) {
+      value = value.trim();
+    } else if (length === 3) {
+      value = `(${value}) `
+    } else if (length < 6) {
+      value = `(${value.substring(0, 3)}) ${value.substring(3, length)}`;
+    } else if (length === 6) {
+      value = `(${value.substring(0, 3)}) ${value.substring(3, length)}-`;
+    } else if (length > 6) {
+      value = `(${value.substring(0, 3)}) ${value.substring(3, 6)}-${value.substring(6, 10)}`
+    }
+    setInputValue(value)
+    setSelection({
+      selectionStart: Math.min(selectionStart, value.length),
+      selectionEnd: Math.min(selectionEnd, value.length)
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <input
+          type="tel"
+          id="phone"
+          maxLength={16}
+          placeholder="mobile number"
+          autoComplete="off"
+          onChange={handleInputChange}
+          value={inputValue}
+          selectionStart={selection.selectionStart}
+          selectionEnd={selection.selectionEnd}
+        />
+        <div><label htmlFor="phone">(123) 456-7890</label></div>
+        <button id="runTestBtn">Run Test Cases</button>
       </header>
     </div>
   );
--- react_js_solution/src/App.test.js
+++ react_js_solution/src/App.test.js
@@ -1,8 +1,131 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+function backspace(element) {
+  let actuallyTyped = element.value;
+  const { selectionStart, selectionEnd } = element;
+
+  const backspaceKey = {
+    key: 'Backspace',
+    code: 8,
+    inputType: 'deleteContentBackward',
+  };
+
+  const sharedEventConfig = {
+    key: backspaceKey.key,
+    charCode: backspaceKey.code,
+    keyCode: backspaceKey.code,
+    which: backspaceKey.code,
+    modifier: backspaceKey.modifier,
+  };
+  const downEvent = fireEvent.keyDown(element, sharedEventConfig);
+
+  if (downEvent) {
+    actuallyTyped = actuallyTyped.substring(0, selectionStart - 1) + actuallyTyped.substring(selectionEnd + 1, actuallyTyped.length) 
+
+    fireEvent.input(element, {
+      target: { value: actuallyTyped, selectionStart, selectionEnd },
+      inputType: backspaceKey.inputType,
+      bubbles: true,
+      cancelable: true,
+    });
+  }
+
+  fireEvent.keyUp(element, sharedEventConfig);
+}
+
+test('renders App', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Run Test Cases/i);
   expect(linkElement).toBeInTheDocument();
+  
+});
+
+test('see if input is there', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  expect(inputElement).toBeInTheDocument();
+});
+
+test('Input should not allow non digits', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: 'abc' } });
+  expect(inputElement.value).toBe('');
+});
+
+test('check for less than 3 digit Input', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: '12' } });
+  expect(inputElement.value).toBe('12');
+});
+
+test('check for 3 digit Input', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: '123' } });
+  expect(inputElement.value).toBe('(123) ');
+});
+
+test('check for less than 6 digit Input', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: '12345' } });
+  expect(inputElement.value).toBe('(123) 45');
+});
+
+test('check for 6 digit Input', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: '123456' } });
+  expect(inputElement.value).toBe('(123) 456-');
+});
+
+test('check for less than 10 digit Input', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: '12345678' } });
+  expect(inputElement.value).toBe('(123) 456-78');
+});
+
+test('check for 10 digit Input', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: '1234567890' } });
+  expect(inputElement.value).toBe('(123) 456-7890');
+});
+
+test('check for more than 10 digit Input', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: '123456789012345' } });
+  expect(inputElement.value).toBe('(123) 456-7890');
+});
+
+test('check for 10 digit Input', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: '1234567890' } });
+  expect(inputElement.value).toBe('(123) 456-7890');
+});
+
+test('check for Delete from last', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: '1234567890' } });
+  backspace(inputElement)
+  expect(inputElement.value).toBe('(123) 456-789');
+});
+
+test('check for Delete from middle', () => {
+  render(<App />);
+  const inputElement = document.getElementById('phone');
+  fireEvent.change(inputElement, { target: { value: '1234567890' } });
+  inputElement.selectionStart = 5
+  inputElement.selectionEnd = 6
+  backspace(inputElement)
+  backspace(inputElement)
+  expect(inputElement.selectionStart).toBe(5);
+  expect(inputElement.selectionEnd).toBe(6);
 });
